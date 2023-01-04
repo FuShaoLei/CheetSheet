ab3fe5608af9bdcd332441f9ea3e965ac58460ee
FuShaoLei <1563250958@qq.com>
2023-01-04 22:41:43 +0800

    update

--- deployToGithub.sh
+++ deployToGithub.sh
@@ -4,7 +4,7 @@ git init
 git remote add origin git@github.com:FuShaoLei/CheetSheet.git
 git add .
 git commit -m 'update blog'
-git push -f --set-upstream origin gh-page
+git push -f --set-upstream origin master:gh-page
 cd ..
 rm -rf public
 git add .
